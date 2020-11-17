3c072c98cb3861e040e6b71ac8a39ffee821e8af
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-17 13:20:36 +0900

    商品一覧機能の修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,11 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :postage
+  belongs_to :shipping
+  belongs_to :status
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
