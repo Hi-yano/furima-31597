88c535e24dc5c7b663eb6c13f5588f7a3b453a53
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-17 21:45:23 +0900

    修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :move_to_index, except: [:index, :search, :show]
-  before_action :set_product, only: [:show, :edit, :update]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all.order('created_at DESC')
@@ -31,8 +31,11 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    product = Product.find(params[:id])
-    redirect_to root_path if product.destroy
+    unless current_user.id == @product.user_id
+      redirect_to root_path
+    else @product.destroy
+      redirect_to root_path
+    end
   end
 
   private
