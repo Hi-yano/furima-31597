1415bdc5ff70d1dbb4144c5a6fd8bb585ac99299
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-21 14:26:45 +0900

    プッシュするための変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+
   before_action :authenticate_user!
   before_action :set_params, only: [:index, :create]
   before_action :move_to_index, only: :index
